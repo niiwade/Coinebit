1ff9ae999356c6a47c52c9a37a2d117f4d13a32c
niiwade <niilantewade77@gmail.com>
2021-09-21 03:39:44 +0000

    push

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+var options = {
+    method: 'GET',
+    url: 'https://coinranking1.p.rapidapi.com/exchanges',
+    headers: {
+      'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+      'x-rapidapi-key': 'a278dc7ca6mshfce57d390473ddep1491d2jsnae801291d6bc'
+    }
+  };
