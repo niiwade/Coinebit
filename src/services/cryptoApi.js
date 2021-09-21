675a93cffdac391c8998bc2ddfc66429fa60c437
niiwade <niilantewade77@gmail.com>
2021-09-21 03:49:34 +0000

    cryptoApi created

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,21 @@
-var options = {
-    method: 'GET',
-    url: 'https://coinranking1.p.rapidapi.com/exchanges',
-    headers: {
-      'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-      'x-rapidapi-key': 'a278dc7ca6mshfce57d390473ddep1491d2jsnae801291d6bc'
-    }
-  };
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'a278dc7ca6mshfce57d390473ddep1491d2jsnae801291d6bc'
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url,headers,cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => {
+    getCryptos: builder.query({
+      query: () => createRequest('/exchanges')
+    }),
+  }
+
+});
