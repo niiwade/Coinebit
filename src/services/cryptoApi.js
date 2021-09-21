2a095350ab4ddebbc30bf38fa01e7e145495716d
niiwade <niilantewade77@gmail.com>
2021-09-21 11:25:56 +0000

    news component and news Api  finished

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,4 +1,4 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query';
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
   'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
@@ -7,15 +7,19 @@ const cryptoApiHeaders = {
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
-const createRequest = (url) => ({url,headers,cryptoApiHeaders})
+const createRequest = (url) => ({url,headers: cryptoApiHeaders})
 
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
   baseQuery: fetchBaseQuery({ baseUrl }),
-  endpoints: (builder) => {
+  endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: () => createRequest('/exchanges')
+      query: (count) => createRequest(`/coins?limit=${count}`)
     }),
-  }
+  })
 
 });
+
+export const { useGetCryptosQuery,
+} = cryptoApi;
+
