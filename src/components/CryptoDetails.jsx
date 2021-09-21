f8066965da7a02bd7997ae5c071a8ee99fdb03b7
niiwade <niilantewade77@gmail.com>
2021-09-21 20:13:27 +0000

    fetching stats for cryptodetails

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -12,7 +12,26 @@ const {Option} = Select;
 const CryptoDetails = () => {
     const { coinId } = useParams();
     const [timePeriod, setTimePeriod] = useState('7d')
-    const [data, isFetching] = useGetCryptoDetailsQuery(coinId, timePeriod);
+    const [data, isFetching] = useGetCryptoDetailsQuery(coinId);
+    
+    const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y', '10y']
+    
+    const stats = [
+        { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails)}` }
+        { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+        { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails)}` }
+        { title: 'Market Cap', value: `$ ${cryptoDetails.marketcap && millify(cryptoDetails)}` }
+        { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh && millify(cryptoDetails)}` }
+    ];
+
+    const genericstats = [
+        { title: 'Number of Markets', value: `$ ${cryptoDetails.numberof  && millify(cryptoDetails)}` }
+        { title: 'Number of Exchanges', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+        { title: 'Approved Supply', value: `$ ${cryptoDetails.volume && millify(cryptoDetails)}` }
+        { title: 'Total Supply', value: `$ ${cryptoDetails.marketcap && millify(cryptoDetails)}` }
+        { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.allTimeHigh && millify(cryptoDetails)}` }
+    ];
+    
     
     return (
         <div>
